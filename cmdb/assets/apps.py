9857fe7e986984f32c8f2d5eca994a8e37aeacc4
SuperLandy <zzq0226@hotmail.com>
2019-07-08 16:21:36 +0800

    1

--- cmdb/assets/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class AssetsConfig(AppConfig):
-    name = 'assets'
--- cmdb/start.sh
+++
@@ -1,2 +0,0 @@
-cd /opt/cmdb/
-nohup python manage.py runserver 0.0.0.0:80 1>/dev/null 2>/dev/null &
